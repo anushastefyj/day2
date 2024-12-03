d9374e71ae1fd9a36c2fe0f794cfc10cf3177bae
anushastefyj <24hr5a3001@mtieat.org>
2024-12-03 15:56:45 +0530

    Add files via upload

---
+++ swapaddsub.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class swapp{
+ public static void main(String args[]){
+
+        Scanner Sc=new Scanner(System.in);
+        {
+        int a=Sc.nextInt();
+        int b=Sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        String res=String.format("after swapping boy=%d and girl=%d",a,b);
+        System.out.println(res);
+    }
+}
+}
