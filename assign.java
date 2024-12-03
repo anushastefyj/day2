efad4089313ec7e8d769fd21c27953bce4438f5b
anushastefyj <24hr5a3001@mtieat.org>
2024-12-03 12:41:15 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,14 @@
+class assign {
+        public static void main(String args[])
+        {
+            int a=30;
+            System.out.println(a+=30);
+            System.out.println(a-=3);
+            System.out.println(a*=30);
+            System.out.println(a/=30);
+            System.out.println(a%=30);
+       
+        }
+    
+    }
+       
