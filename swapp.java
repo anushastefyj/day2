e633b1327224523c7f40f4810b456eeffaef33c9
anushastefyj <24hr5a3001@mtieat.org>
2024-12-03 12:41:41 +0530

    Add files via upload

---
+++ swapp.java
@@ -0,0 +1,16 @@
+
+ class swap{
+ public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        {
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        String res=String.format("after swapping lnboy=%dlngirl=%d",a,b);
+        System.out.println(res);
+    }
+}
+}
