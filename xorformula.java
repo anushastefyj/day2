963f3d20c3def7966d70247693727c8110fe63e2
anushastefyj <24hr5a3001@mtieat.org>
2024-12-03 16:19:35 +0530

    Add files via upload

---
+++ xorformula.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class xorformula {
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        int b=Sc.nextInt();
+        System.out.println(a^b);
+        System.out.println(a^b-(a&b));
+        {
+            if(a==b)
+            {
+                System.out.println("resultant is 0");
+            }
+        else
+    {
+        System.out.println("resultant varies");
+    }}
+    }
+    }
+    
